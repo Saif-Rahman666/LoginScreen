21c7ed32a75ee501eac9a4f7106790386b5292c2
Saif Rahman <Saf.saif@gmail.com>
2023-10-28 15:15:51 +0600

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Saif Rahman on 28/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
