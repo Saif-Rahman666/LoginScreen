d98d61add1e86f04a8ecf6dc1e33602d668f4a86
Saif Rahman <Saf.saif@gmail.com>
2023-11-13 20:22:18 +0600

    Login Screen

--- HelloWorld/ContentView.swift
+++ HelloWorld/ContentView.swift
@@ -8,14 +8,79 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var username = ""
+    @State private var password = ""
+    @State private var wrongUsername = 0
+    @State private var wrongPassword = 0
+    @State private var showingLoginScreen = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ZStack{
+                Color.green
+                    .ignoresSafeArea()
+                Circle()
+                    .scale(1.7)
+                    .foregroundColor(.white.opacity(0.15))
+                Circle()
+                    .scale(1.35)
+                    .foregroundColor(.white.opacity(0.15))
+                VStack{
+                    Text("Login")
+                        .font(.largeTitle)
+                        .bold()
+                        .padding()
+                    TextField("Username", text: $username)
+                        .padding()
+                        .frame(width: 300, height: 50 )
+                        .background(Color.black.opacity(0.10))
+                        .cornerRadius(10)
+                        .border(.red, width: CGFloat(wrongUsername))
+                    SecureField("Password", text: $password)
+                        .padding()
+                        .frame(width: 300, height: 50 )
+                        .background(Color.black.opacity(0.10))
+                        .cornerRadius(10)
+                        .border(.red, width: CGFloat(wrongPassword))
+                    Button("Login"){
+                        authenticateUser(username: username, password: password)
+                    }
+                    .foregroundColor(.white)
+                    .frame(width: 300, height: 50)
+                    .background(Color.green)
+                    .cornerRadius(10)
+                    
+                    NavigationLink(destination: Text("You are welcome @\(username)"), isActive: $showingLoginScreen) {
+                        EmptyView()
+                    }
+                }
+            }
+            .navigationBarHidden(true)
+        }
+        
+    }
+    
+    func authenticateUser(username: String, password: String) {
+        if username.lowercased() == "saif666" {
+            wrongUsername = 0
+            if password.lowercased() == "qwer" {
+                wrongPassword = 0
+                showingLoginScreen = true
+            }else {
+                wrongPassword = 2
+            }
+        }else {
+            wrongUsername = 2
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .previewInterfaceOrientation(.portrait)
+           
+        }
     }
 }
